19b565a1b8074c2b0f1793c3978e1c390421bfc8
Jose Luis <jlsierrabenito@gmail.com>
2020-04-23 14:02:46 +0200

    Eliminado mas ficheros innecesarios

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -6,12 +6,7 @@
 
 1. Vars and Inits
 2. Set Header
-3. Init Menu
-4. Init Home Slider
-5. Init Scrolling
-6. Init Isotope
-7. Init Testimonials Slider
-8. Init Input
+3. Init Home Slider
 
 
 ******************************/
@@ -27,11 +22,7 @@ $(document).ready(function()
 	*/
 
 	var header = $('.header');
-	var headerSocial = $('.header_social');
-	var menu = $('.menu');
-	var menuActive = false;
-	var burger = $('.hamburger');
-
+	
 	setHeader();
 
 	$(window).on('resize', function()
@@ -49,12 +40,7 @@ $(document).ready(function()
 		setHeader();
 	});
 
-	initMenu();
 	initHomeSlider();
-	initIsotope();
-	initTestimonialsSlider();
-	initScrolling();
-	initInput();
 
 	/* 
 
@@ -67,64 +53,11 @@ $(document).ready(function()
 		if($(window).scrollTop() > 127)
 		{
 			header.addClass('scrolled');
-			headerSocial.addClass('scrolled');
 		}
 		else
 		{
 			header.removeClass('scrolled');
-			headerSocial.removeClass('scrolled');
 		}
-	}
-
-	/* 
-
-	3. Set Menu
-
-	*/
-
-	function initMenu()
-	{
-		if($('.menu').length)
-		{
-			var menu = $('.menu');
-			if($('.hamburger').length)
-			{
-				burger.on('click', function()
-				{
-					if(menuActive)
-					{
-						closeMenu();
-					}
-					else
-					{
-						openMenu();
-					}
-				});
-			}
-		}
-		if($('.menu_close').length)
-		{
-			var close = $('.menu_close');
-			close.on('click', function()
-			{
-				if(menuActive)
-				{
-					closeMenu();
-				}
-			});
-		}
-	}
-
-	function openMenu()
-	{
-		menu.addClass('active');
-		menuActive = true;
-	}
-
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		menuActive = false;
 	}
 
 	/* 
@@ -150,125 +83,4 @@ $(document).ready(function()
 		}
 	}
 
-	/* 
-
-	5. Init Scrolling
-
-	*/
-
-	function initScrolling()
-	{
-		if($('.home_page_nav ul li a').length)
-		{
-			var links = $('.home_page_nav ul li a');
-	    	links.each(function()
-	    	{
-	    		var ele = $(this);
-	    		var target = ele.data('scroll-to');
-	    		ele.on('click', function(e)
-	    		{
-	    			e.preventDefault();
-	    			$(window).scrollTo(target, 1500, {offset: -90, easing: 'easeInOutQuart'});
-	    		});
-	    	});
-		}	
-	}
-
-	/* 
-
-	6. Init Isotope
-
-	*/
-
-	function initIsotope()
-	{
-		if($('.item_grid').length)
-		{
-			var grid = $('.item_grid').isotope({
-				itemSelector: '.item',
-	            getSortData:
-	            {
-	            	price: function(itemElement)
-	            	{
-	            		var priceEle = $(itemElement).find('.destination_price').text().replace( 'From $', '' );
-	            		return parseFloat(priceEle);
-	            	},
-	            	name: '.destination_title a'
-	            },
-	            animationOptions:
-	            {
-	                duration: 750,
-	                easing: 'linear',
-	                queue: false
-	            }
-	        });
-		}
-	}
-
-	/* 
-
-	7. Init Testimonials Slider
-
-	*/
-
-	function initTestimonialsSlider()
-	{
-		if($('.testimonials_slider').length)
-		{
-			var testSlider = $('.testimonials_slider');
-			testSlider.owlCarousel(
-			{
-				animateOut: 'fadeOut',
-    			animateIn: 'flipInX',
-				items:1,
-				autoplay:true,
-				loop:true,
-				smartSpeed:1200,
-				dots:false,
-				nav:false
-			});
-		}
-	}
-
-	/* 
-
-	8. Init Input
-
-	*/
-
-	function initInput()
-	{
-		if($('.newsletter_input').length)
-		{
-			var inpt = $('.newsletter_input');
-			inpt.each(function()
-			{
-				var ele = $(this);
-				var border = ele.next();
-
-				ele.focus(function()
-				{
-					border.css({'visibility': "visible", 'opacity': "1"});
-				});
-				ele.blur(function()
-				{
-					border.css({'visibility': "hidden", 'opacity': "0"});
-				});
-
-				ele.on("mouseenter", function()
-				{
-					border.css({'visibility': "visible", 'opacity': "1"});
-				});
-
-				ele.on("mouseleave", function()
-				{
-					if(!ele.is(":focus"))
-					{
-						border.css({'visibility': "hidden", 'opacity': "0"});
-					}
-				});
-				
-			});
-		}
-	}
 });
