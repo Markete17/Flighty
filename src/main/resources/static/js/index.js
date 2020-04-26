912a859598bb27707d86bc6a8ca923e81206e5c9
Jose Luis <jlsierrabenito@gmail.com>
2020-04-26 17:38:31 +0200

    Html se añade dinamicamente

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -48,7 +48,7 @@ $( document ).ready(function() {
 
 	*/
     
-    //Show/hide input of return flight 
+    // Show/hide input of return flight 
     $('input[name=method]').on('change', function(){
     	var returnDate = $('#date2');
     	if ($('#round_trip').is(':checked')) {
@@ -64,7 +64,7 @@ $( document ).ready(function() {
     	}
     });
     
-    //On blur set to text type if empty or update min value of return flight if round trip
+    // On blur set to text type if empty or update min value of return flight if round trip
     $('#date1').blur(function() {
     	var outboundDate = $('#date1');
     	if (!outboundDate.val()) {
@@ -75,12 +75,12 @@ $( document ).ready(function() {
     	}
     });
     
-    //On focus set type to date
+    // On focus set type to date
     $('#date1').focus(function() {
     	$('#date1').prop('type', 'date');
     });
     
-    //On blur set to text type if empty
+    // On blur set to text type if empty
     $('#date2').blur(function() {
     	var returnDate = $('#date2');
     	if (!returnDate.val()) {
@@ -88,7 +88,7 @@ $( document ).ready(function() {
     	} 
     });
     
-    //On focus set type to date
+    // On focus set type to date
     $('#date2').focus(function() {
     	$('#date2').prop('type', 'date');
     });
@@ -100,13 +100,108 @@ $( document ).ready(function() {
 	*/
     
     $('#home_search_form').on('submit', function(e) {
+    	// Not load page
     	e.preventDefault();
+    	
+    	// Get flights with the search criteria
     	var url = $(this).attr('action') + '?' + $(this).serialize();
+    	
+    	if (!$('#date2').val()) {
+    		url = url + encodeURI('1927-03-14'); //Avoid error in backend
+    	}
+    	
     	$.getJSON(url, function (response) {
-    			alert('Perfect');
+    		$('#not_found').text('');
+    		$('#list_flights').append(
+				["<h2 class='w-100 text-center pb-3'>",
+					"<span class='text-dark'>7 July Tuesday - 14 July Tuesday</span>",
+				"</h2>",
+				"<div class='row d-sm-flex justify-content-between mt-4 mb-3 ml-4 mr-4 border-bottom'>",
+					"<!-- Outgoing flight -->",
+					"<div class='d-block text-center text-sm-left'>",
+						"<div class='row'>",
+							"<img class='img_icon mr-sm-2' src='/images/plane_orig.png'",
+								"alt='plane taking off'>",
+							"<table class='d-flex table-sm text-center'>",
+								"<tbody>",
+									"<tr>",
+										"<th><small>Madrid (Barajas-Adolfo Suarez)</small></th>",
+										"<th></th>",
+										"<th><small>Barcelona</small></th>",
+									"</tr>",
+									"<tr>",
+										"<th><h3 class='text-dark'>07:30</h3></th>",
+										"<th><h3 class='text-dark mx-3'>-</h3></th>",
+										"<th><h3 class='text-dark'>08:55</h3></th>",
+									"</tr>",
+								"</tbody>",
+							"</table>",
+							"<img class='img_icon ml-sm-1' src='/images/plane_dest.png'",
+								"alt='plane landing'>",
+						"</div>",
+						"<div class='row d-block text-center'>",
+							"<h4 class='text-primary'>235€</h4>",
+							"<h6>",
+								"<span class='font-weight-bold'>Duration:</span> 173 min",
+							"</h6>",
+							"<h6>",
+								"<span class='font-weight-bold'>Company:</span> Cathay Pacific",
+									"Airways",
+							"</h6>",
+						"</div>",
+					"</div>",
+					"<!-- Image between outcoming & incoming flight -->",
+					"<div class='d-block mx-auto mt-2'>",
+						"<div class='row'>",
+							"<img class='img_icon' src='/images/arrows.png'",
+								"alt='two arrows'>",
+						"</div>",
+						"<div class='row d-block text-center mt-3'>",
+							"<h3 class='text-primary'>235€</h3>",
+						"</div>",
+					"</div>",
+					"<!-- Incoming flight -->",
+					"<div class='d-block text-center text-sm-left'>",
+						"<div class='row'>",
+							"<img class='img_icon mr-sm-2' src='/images/plane_orig.png'",
+								"alt='plane taking off'>",
+							"<table class='d-flex table-sm text-center'>",
+								"<tbody>",
+									"<tr>",
+										"<th><small>Madrid (Barajas-Adolfo Suarez)</small></th>",
+										"<th></th>",
+										"<th><small>Barcelona</small></th>",
+									"</tr>",
+									"<tr>",
+										"<th><h3 class='text-dark'>07:30</h3></th>",
+										"<th><h3 class='text-dark mx-3'>-</h3></th>",
+										"<th><h3 class='text-dark'>08:55</h3></th>",
+									"</tr>",
+								"</tbody>",
+							"</table>",
+							"<img class='img_icon ml-sm-1' src='/images/plane_dest.png'",
+								"alt='plane landing'>",
+						"</div>",
+						"<div class='row d-block text-center'>",
+							"<h4 class='text-primary'>235€</h4>",
+							"<h6>",
+								"<span class='font-weight-bold'>Duration:</span> 173 min",
+							"</h6>",
+							"<h6>",
+								"<span class='font-weight-bold'>Company:</span> Cathay Pacific",
+								"Airways",
+							"</h6>",
+						"</div>",
+					"</div>",
+				"</div>"].join(""));
     	}).fail(function() {
-    		
+    		$('#not_found').text('No flights found');
     	});
+    	
+    	// Scroll to results
+		$([document.documentElement, document.body]).animate({
+	        scrollTop: $(".intro").offset().top
+	    }, 2000);
     });
         
 });
